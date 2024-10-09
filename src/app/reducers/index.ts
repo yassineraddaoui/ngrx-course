8da58c363ddc96109abf8e584cd7879cc68c1c72
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-10-09 15:08:28 +0100

    trigger logger meta reducer only after certain Actions (login & logout)  + meta reducers notes

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -3,32 +3,32 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
-import {routerReducer} from '@ngrx/router-store';
-
-export interface AppState {
-
-}
+  MetaReducer,
+} from "@ngrx/store";
+import { environment } from "../../environments/environment";
+import { routerReducer } from "@ngrx/router-store";
+import { AuthActions } from "../auth/action-types";
+export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = {
-    router: routerReducer
+  router: routerReducer,
 };
 
-export function logger(reducer:ActionReducer<any>)
-    : ActionReducer<any> {
-    return (state, action) => {
-        console.log("state before: ", state);
-        console.log("action", action);
-
-        return reducer(state, action);
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    if (LOGGABLE_ACTIONS.includes(action.type)) {
+      console.log("state before: ", state);
+      console.log("action", action);
     }
-
+    return reducer(state, action);
+  };
 }
 
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? [logger]
+  : [];
 
-export const metaReducers: MetaReducer<AppState>[] =
-    !environment.production ? [logger] : [];
-
-
+const LOGGABLE_ACTIONS :string[] = [
+  AuthActions.login.type,
+  AuthActions.logout.type
+];
